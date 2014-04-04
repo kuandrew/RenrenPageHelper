7cc14858aba075b3d1d56b543cd6351b1cee2d40
Ku_Andrew <kungg@kungg.com>
2014-04-04 17:43:31 +0800

    Update config.php

--- Conf/config.php
+++ Conf/config.php
@@ -5,8 +5,8 @@ $config = array(
     'URL_MODEL' => 1,
     'URL_CASE_INSENSITIVE' =>false,//url大小写
     'URL_HTML_SUFFIX'=>'',//伪静态后缀
-    'SITE_URL'  => 'http://page.kungg.com/App/',
-    'ROOT_URL'  => 'http://page.kungg.com',
+    'SITE_URL'  => '',
+    'ROOT_URL'  => '',
     'Wait_Second_Csrf' => 3, //页面跳转等待时间
     /**
      * 数据库配置
@@ -33,7 +33,7 @@ $config = array(
     /**
      * 加密用的Key
      */
-    'Secret_Key'    => 'asdt=02,,2sxxs1',
+    'Secret_Key'    => '',
     /**
      * 
      */
@@ -41,20 +41,20 @@ $config = array(
     /**
      * 人人开放平台
      */
-    'RENREN_APP_KEY'    => '8da3d03b34794e0bb061a004056fff92',
-    'RENREN_APP_SECRET' => '145859bc81974dab8b8eaeb34dd20c11',
-    'RENREN_APP_ID'     => '235544',
+    'RENREN_APP_KEY'    => '',
+    'RENREN_APP_SECRET' => '',
+    'RENREN_APP_ID'     => '',
     'RENREN_CALLBACK'   => 'Renren',
     'RENREN_SCOPE'      => 'read_user_album+read_user_feed+status_update+admin_page+photo_upload',
     'RENREN_CALLBACK_TWICE'   => 'Renren/reg_twice', //已经注册却没有绑定人人，使用回调地址
     'RENREN_UPLOAD_DIR' => APP_PATH.'pub/upload/',  //上传图片时的本地目录
-    'RENREN_PUBLIC_TOKEN' => 'ksldjf9JMsd93Asdf', //人人网公众号TOKEN
+    'RENREN_PUBLIC_TOKEN' => '', //人人网公众号TOKEN
     /**
      * 公众平台
      */
 <<<<<<< HEAD
-    //'WX_TOKEN'       => '90fdlmlwease233',
-    'RENREN_TOKEN'   => 'ksldjf9JMsd93Asdf',
+    //'WX_TOKEN'       => '',
+    'RENREN_TOKEN'   => '',
 =======
     //'WX_TOKEN'       => '',
     'RENREN_TOKEN'   => '',
